f17a2fc84638347141c599cb3b52de77663211f5
CSCD13 <carlossantiagocruz11@gmail.com>
2026-09-24 18:10:31 -0600

    Connected all buttons to their character and minor polishing

---
+++ Assets/Scripts/Character2Changer.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Character3Changer : MonoBehaviour
+{
+    [SerializeField] private GameObject newPlayerPrefab;
+
+    public void SwapPlayer() {
+        GameObject currentPlayer = GameObject.FindGameObjectWithTag("Player");
+
+        if (currentPlayer != null && newPlayerPrefab != null) {
+            Instantiate(newPlayerPrefab, currentPlayer.transform.position, currentPlayer.transform.rotation);
+            Destroy(currentPlayer);
+        } 
+    }
+}
